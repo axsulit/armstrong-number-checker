3e4112db4e79e170e3c524044c259c68cddc1365
Anne <anne_sulit@dlsu.edu.ph>
2024-03-01 19:48:26 +0800

    base pseudocode

--- SuliAToriY.asm
+++ SuliAToriY.asm
@@ -5,6 +5,24 @@
 section .text
 global main
 main:
-    ;write your code here
+    ; Get input from user.
+    
+    ; Check if the input is a valid integer and positive.
+        ; call error func
+        
+    ; Calculate the number of digits in the input number.
+    
+    ; Loop through each digit of the number.
+        ; calculate its nth power, 
+        ; add it to the total sum;
+
+    ; Compare the total sum to the input number to determine if it's an Armstrong number;
+    
+    ; display result
+    
+    ; prompt to start again
+    
     xor rax, rax
     ret
+    
+; error function - show error message
